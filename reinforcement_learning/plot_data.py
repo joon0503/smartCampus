3b5bdd256b33f2467a32743bdf46e774c9377c33
Seungjoon Lee <sjoon0503@gmail.com>
2018-10-08 11:11:43 +0900

    Fixed not plotting y-values correctly. Also added input to specify end episode.

--- reinforcement_learning/plot_data.py
+++ reinforcement_learning/plot_data.py
@@ -19,9 +19,12 @@ parser = ArgumentParser(
     description='File for learning'
     )
 parser.add_argument('--VEH_POS', type=str, help='path to vehicle position data')
+parser.add_argument('--VEH_RATE', type=int, help='sampling rate for vehicle', default=100 )
+parser.add_argument('--END_EPI', type=int, help='last epsidoe to plot')
 parser.add_argument('--REW', type=str, help='path to reward data')
-
 options = parser.parse_args()
+
+
 
 #################################
 # GRAPH VEHICLE POSITION
@@ -31,11 +34,17 @@ if os.path.isfile(options.VEH_POS):
     # Load
     veh_data = pickle.load( open( options.VEH_POS, "rb" ) )
 
+    # Check option
+    if options.END_EPI == None or options.END_EPI > veh_data.shape[0]:
+        options.END_EPI = veh_data.shape[0]
+
+    
     # Plot
     plt.figure(0)
 
-    for i in range(0, veh_data.shape[0]):           # For each episode
-       plt.scatter(veh_data[i][:,0], veh_data[i][:,0], label = "Trail #" + str(i)) 
+    for i in range(0, options.END_EPI, options.VEH_RATE):           # For each episode
+#        plt.scatter(veh_data[i][:,0], veh_data[i][:,1], label = "Trail #" + str(i)) 
+        plt.plot(veh_data[i][:,0], veh_data[i][:,1], '-o', label = "Trail #" + str(i)) 
 
     # Plot Map
     plt.plot([1.25, 1.25],[0, 60],'k')
