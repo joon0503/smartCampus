c909c99862057080313f9f77edb5e4026201a39b
Seungjoon Lee <sjoon0503@gmail.com>
2019-03-11 15:03:30 +0900

    Adding another utils files for general utility functions.

--- reinforcement_learning/basic_dqn.py
+++ reinforcement_learning/basic_dqn.py
@@ -18,6 +18,7 @@ from experience_replay import SumTree
 from experience_replay import Memory
 from argparse import ArgumentParser
 from utils_vrep import *            # import everything directly from utils_vrep.py
+from utils_training import *            # import everything directly from utils_vrep.py
  
 MAX_DISTANCE = 20
 COLLISION_DISTANCE = 1.3
@@ -314,69 +315,6 @@ class QAgent:
 
 
 
-# Detect whether vehicle is collided
-# For now simply say vehicle is collided if distance if smaller than 1.1m. Initial sensor distance to the right starts at 1.2m.
-# Input
-#   dDistance: array of sensor measurement
-#   dState   : array of sensor detections state
-# Output
-#   T/F      : whether collision occured
-#   sensor # : Which sensor triggered collision. -1 if no collision
-def detectCollision(dDistance):
-    for i in range(SENSOR_COUNT):
-        if dDistance[i]*MAX_DISTANCE < COLLISION_DISTANCE:
-            return True, i
-    
-    return False, -1    
-
-# Get goal point vector. Returns angle and distance
-# Output
-# goal_angle: angle to the goal point in radians / pi
-#             Positive angle mean goal point is on the right of vehicle, negative mean goal point is on the left
-# goal_distance: distance to the goal point / MAX_DISTANCE
-def getGoalPoint( veh_index ):
-    _, vehPos = vrep.simxGetObjectPosition( clientID, vehicle_handle[veh_index], -1, vrep.simx_opmode_blocking)            
-    _, dummyPos = vrep.simxGetObjectPosition( clientID, dummy_handle[veh_index], -1, vrep.simx_opmode_blocking)            
-
-    # Calculate the distance
-    delta_distance = np.array(dummyPos) - np.array(vehPos)              # delta x, delta y
-    goal_distance = np.linalg.norm(delta_distance)
-
-    # calculate angle
-    goal_angle = math.atan( delta_distance[0]/delta_distance[1] )       # delta x / delta y
-    goal_angle = goal_angle / math.pi                               # result in -1 to 1
-
-    return goal_angle, goal_distance / GOAL_DISTANCE
-
-# veh_pos_info: (options.VEH_COUNT x 2)
-def getGoalInfo( veh_pos_info, goal_pos_info ):
-    # Calculate the distance
-    delta_distance = goal_pos_info - veh_pos_info              # delta x, delta y
-    #print(goal_pos_info)
-    #print(veh_pos_info)
-    #print(delta_distance)
-    goal_distance = np.linalg.norm(delta_distance, axis=1)
-
-    # calculate angle
-    goal_angle = np.arctan( delta_distance[:,0]/delta_distance[:,1] )       # delta x / delta y
-    goal_angle = goal_angle / math.pi                               # result in -1 to 1
-
-    return goal_angle, goal_distance / GOAL_DISTANCE
-
-
-
-# Detect whether vehicle reached goal point.
-# Input
-#   vehPos - [x,y,z] - center of vehicle. To get tip, add 2.075m
-#   gInfo  - [angle,distance]
-# Output
-#   True/False
-def detectReachedGoal(vehPos, gInfo, currHeading):
-    # Distance less than 0.5m, angle less than 10 degrees
-    if abs(gInfo[1]*GOAL_DISTANCE - 2.075) < 1.0 and abs(currHeading*90)<5: 
-        return True
-    else:
-        return False
 
 
 # Get current state of the vehicle. It is combination of different information
