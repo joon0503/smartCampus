5b40456bc190190cb60b041eb40f1bac764d0ac5
Seungjoon Lee <sjoon0503@gmail.com>
2019-05-01 17:43:41 +0900

    Adjusting sensor height

--- scenario_generation/genSceneExtended.py
+++ scenario_generation/genSceneExtended.py
@@ -128,7 +128,7 @@ for i in range(0,COPY_NUM):
         print(str(s))
         err_code1, sensor_handle = vrep.simxCopyPasteObjects(clientID,[sensor_handle_array[0]], vrep.simx_opmode_blocking)
         vrep.simxSetObjectParent(clientID,sensor_handle[0],vehicle_handle,False,vrep.simx_opmode_blocking)
-        ret_code = vrep.simxSetObjectPosition(clientID,sensor_handle[0],vehicle_handle,[1.5,0,0.7],vrep.simx_opmode_blocking)
+        ret_code = vrep.simxSetObjectPosition(clientID,sensor_handle[0],vehicle_handle,[1.5,0,0.1],vrep.simx_opmode_blocking)
         vrep.simxSetObjectOrientation(clientID,sensor_handle[0],vehicle_handle,[-math.pi/2,RAD_DT*s,0],vrep.simx_opmode_oneshot)
         sensor_handle_array = np.append(sensor_handle_array,sensor_handle)   
 
