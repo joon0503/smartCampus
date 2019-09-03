48da2b095e6ae4e21ec83ef64ebb17c195e420db
Seungjoon Lee <sjoon0503@gmail.com>
2019-09-03 10:40:00 +0900

    Removed unused code.

--- reinforcement_learning/dqn_bullet.py
+++ reinforcement_learning/dqn_bullet.py
@@ -155,54 +155,6 @@ def get_options():
 
     return parser, options
 
-
-##################################
-# TENSORFLOW HELPER
-##################################
-
-# def saveNetworkKeras():
-#     print('-----------------------------------------')
-#     print("Saving network...")
-#     print('-----------------------------------------')
-#     if not os.path.exists('./checkpoints-vehicle'):
-#         os.makedirs('./checkpoints-vehicle')
-#     agent_train.model.save_weights('./checkpoints-vehicle/' + START_TIME_STR + "_e" + str(epi_counter) + "_gs" + str(global_step) + '.h5', overwrite=True)
-
-#     # Save checkpoint
-#     with open('./checkpoints-vehicle/checkpoint.txt','w') as check_file:
-#         check_file.write(START_TIME_STR + "_e" + str(epi_counter) + "_gs" + str(global_step) + '.h5')
-
-#     return
-
-# def loadNetworkKeras():
-#     weight_path = None
-
-#     # If file is provided
-#     if options.WEIGHT_FILE != None:
-#         weight_path = options.WEIGHT_FILE
-#     elif os.path.isfile('./checkpoints-vehicle/checkpoint.txt'):
-#         with open('./checkpoints-vehicle/checkpoint.txt') as check_file:
-#             weight_file = check_file.readline()
-#             weight_path = './checkpoints-vehicle/' + weight_file 
-
-#     if weight_path == None:
-#         print("\n\n=================================================")
-#         print("=================================================")
-#         print("Could not find old network weights")
-#         print("=================================================")
-#         print("=================================================\n\n")
-#     else:
-#         agent_train.model.load_weights( weight_path )
-#         agent_target.model.load_weights( weight_path )
-#         print("\n\n=================================================")
-#         print("=================================================")
-#         print("Successfully loaded:", weight_path)
-#         print("=================================================")
-#         print("=================================================\n\n")
-
-#     time.sleep(2)
-#     return
-
 ########################
 # MAIN
 ########################
@@ -265,8 +217,6 @@ if __name__ == "__main__":
     # TF Setup
     ##############
     q_algo          = dqn( sim_env )
-    # agent_train     = QAgent(options,sim_env.scene_const, 'Training')
-    # agent_target    = QAgent(options,sim_env.scene_const, 'Target')
     agent_icm       = ICM(options,sim_env.scene_const,'icm_Training')
 
     # saving and loading networks
@@ -338,7 +288,6 @@ if __name__ == "__main__":
             time.sleep(0.01)
 
         # Decay epsilon
-        # agent_train.decayEps( options, global_step )
         q_algo.agent_train.decayEps( options, global_step )
         global_step += options.VEH_COUNT
 
@@ -359,23 +308,8 @@ if __name__ == "__main__":
             ic(sim_env.sensor_queue, obs_sensor_stack)
             ic(sim_env.goal_queue, obs_goal_stack)
 
-        # Get optimal action Keras. 
-        # action_stack_k = agent_train.sample_action_k(
-        #                                     {
-        #                                         'observation_sensor_k' : obs_sensor_stack,
-        #                                         'observation_goal_k'   : obs_goal_stack,
-        #                                     },
-        #                                     options,
-        #                                     )
-        
         # Get optimal action q_algo
         targetSteer_k, action_stack_k = q_algo.getOptimalAction(obs_sensor_stack, obs_goal_stack)
-
-        # ic(action_stack_k)
-
-        # Apply the Steering Action & Keep Velocity. For some reason, +ve means left, -ve means right
-        # # targetSteer = sim_env.scene_const.max_steer - action_stack * abs(sim_env.scene_const.max_steer - sim_env.scene_const.min_steer)/(options.ACTION_DIM-1)
-        # targetSteer_k = sim_env.scene_const.max_steer - action_stack_k * abs(sim_env.scene_const.max_steer - sim_env.scene_const.min_steer)/(options.ACTION_DIM-1)
 
         # Apply Action
         sim_env.applyAction( targetSteer_k )
@@ -469,93 +403,10 @@ if __name__ == "__main__":
         # Start training
         if global_step >= options.MAX_EXPERIENCE and options.TESTING == False:
             for tf_train_counter in range(0,options.VEH_COUNT):
-                # q_algo
+                ##############################
+                # Train Algorithm
+                ##############################
                 loss_k, states_sensor_mb, next_states_sensor_mb, states_goal_mb, next_states_goal_mb, actions_mb = q_algo.trainOneStep()
-
-                # # Obtain the mini batch. (Batch Memory is '2D array' with BATCH_SIZE X size(experience)
-                # tree_idx, batch_memory, ISWeights_mb = replay_memory.sample(options.BATCH_SIZE)
-
-                # # Get state/action/next state from obtained memory. Size same as queues
-                # states_sensor_mb        = np.array([each[0][0] for each in batch_memory])           # BATCH_SIZE x SENSOR_COUNT
-                # states_goal_mb          = np.array([each[0][1] for each in batch_memory])           # BATCH_SIZE x 2
-                # actions_mb              = np.array([each[0][2] for each in batch_memory])           # BATCH_SIZE x ACTION_DIM
-                # rewards_mb              = np.array([each[0][3] for each in batch_memory])           # 1 x BATCH_SIZE
-                # next_states_sensor_mb   = np.array([each[0][4] for each in batch_memory])   
-                # next_states_goal_mb     = np.array([each[0][5] for each in batch_memory])   
-                # done_mb                 = np.array([each[0][6] for each in batch_memory])   
-
-                # # actions mb is list of numbers. Need to change it into one hot encoding
-                # actions_mb_hot = np.zeros((options.BATCH_SIZE,options.ACTION_DIM))
-                # actions_mb_hot[np.arange(options.BATCH_SIZE),np.asarray(actions_mb, dtype=int)] = 1
-
-                # # actions converted to value array
-                # #actions_mb_val = oneHot2Angle( actions_mb_hot, sim_env.scene_const, options, radians = False, scale = True )
-
-                # # ic(states_sensor_mb, states_goal_mb, actions_mb, rewards_mb, next_states_sensor_mb, next_states_goal_mb, done_mb)
-                # # ic( next_states_mb.reshape(-1,sim_env.scene_const.sensor_count+2,options.FRAME_COUNT) )
-
-                # # Get Target Q-Value
-                # # feed.clear()
-                # # feed.update({agent_train.obs_sensor : next_states_sensor_mb, agent_train.obs_goal : next_states_goal_mb})
-
-                # # Calculate Target Q-value. Uses double network. First, get action from training network
-                # # action_train = np.argmax( agent_train.output.eval(feed_dict=feed), axis=1 )
-                # action_train_k = agent_train.model_out.predict(
-                #                                     {
-                #                                         'observation_sensor_k' : next_states_sensor_mb,
-                #                                         'observation_goal_k'   : next_states_goal_mb
-                #                                     },
-                #                                     batch_size = options.VEH_COUNT
-                # )
-                # action_train_k = np.argmax( action_train_k, axis=1)
-                # # ic(np.argmax(action_train_k,axis=1))
-
-                # if options.disable_DN == False:
-                #     # feed.clear()
-                #     # feed.update({agent_target.obs_sensor : next_states_sensor_mb, agent_target.obs_goal : next_states_goal_mb})
-
-                #     keras_feed = {}
-                #     keras_feed.clear()
-                #     keras_feed.update(
-                #         {
-                #             'observation_sensor_k' : next_states_sensor_mb,
-                #             'observation_goal_k'   : next_states_goal_mb
-                #         }
-
-                #     )
-                #     # Using Target + Double network
-                #     # ic( agent_target.output.eval( feed_dict = feed), agent_target.output.eval( feed_dict = feed).shape  )
-                #     # ic( agent_target.h_s1.eval( feed_dict = feed), agent_target.h_s1.eval( feed_dict = feed).shape  )
-                #     # ic( agent_target.output.eval(feed_dict=feed)[np.arange(0,options.BATCH_SIZE),action_train] )
-                #     # q_target_val = rewards_mb + options.GAMMA * agent_target.output.eval(feed_dict=feed)[np.arange(0,options.BATCH_SIZE),action_train]
-                #     q_target_val_k = rewards_mb + options.GAMMA * agent_target.model_out.predict(keras_feed)[np.arange(0,options.BATCH_SIZE),action_train_k]
-                # else:
-                #     keras_feed.clear()
-                #     keras_feed.update(
-                #         {
-                #             'observation_sensor_k' : next_states_sensor_mb,
-                #             'observation_goal_k'   : next_states_goal_mb
-                #         }
-
-                #     )
-                #     # Just using Target Network.
-                #     q_target_val = rewards_mb + options.GAMMA * np.amax( agent_target.output.eval(feed_dict=feed), axis=1)
-                #     q_target_val_k = rewards_mb + options.GAMMA * np.amas( agent_target.model.predict(keras_feed), axis=1)
-                #     ic(q_target_val_k)
-           
-                # # set q_target to reward if episode is done
-                # for v_mb in range(0,options.BATCH_SIZE):
-                #     if done_mb[v_mb] == 1:
-                #         # q_target_val[v_mb] = rewards_mb[v_mb]
-                #         q_target_val_k[v_mb] = rewards_mb[v_mb]
-
-                # # Train Keras Model
-                # keras_feed = {}
-                # keras_feed.clear()
-                # keras_feed.update({ 'observation_sensor_k' : states_sensor_mb, 'observation_goal_k' : states_goal_mb})
-                # # ic(keras_feed)
-                # # ic(np.reshape(q_target_val_k,(options.BATCH_SIZE,1)))
-                # loss_k = agent_train.model.train_on_batch( keras_feed, np.reshape(q_target_val_k,(options.BATCH_SIZE,1)) )
 
                 ##############################
                 # Train forward model
@@ -579,9 +430,6 @@ if __name__ == "__main__":
 
                 # Save LOSS
                 data_package.add_loss( loss_k )
-
-                # Update priority
-                # replay_memory.batch_update(tree_idx)
         elif global_step < options.MAX_EXPERIENCE:
             # If just running to get memory, do not increment counter
             epi_counter = 0
@@ -596,7 +444,6 @@ if __name__ == "__main__":
             print('-----------------------------------------')
             print("Updating Target network.")
             print('-----------------------------------------')
-            # agent_target.model.set_weights( agent_train.model.get_weights() )
             q_algo.agent_target.model.set_weights( q_algo.agent_train.model.get_weights() )
 
         # Print Rewards
@@ -625,7 +472,6 @@ if __name__ == "__main__":
         if options.TESTING == False:
             if options.NO_SAVE == False and epi_counter - last_saved_epi >= options.SAVER_RATE:
                 q_algo.saveNetworkKeras(START_TIME_STR, epi_counter, global_step)
-                # saveNetworkKeras()
                 print('-----------------------------------------')
                 print("Saving data...") 
                 print('-----------------------------------------')
